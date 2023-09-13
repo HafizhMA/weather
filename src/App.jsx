dc379c238908a133dd86178761f3ab4c5766f490
HafizhMA <muhammadazhr29@gmail.com>
2023-09-13 19:11:42 +0700

    body bg color

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <div className="ts">
-        <h3>Halooooooooooooooooooooooooo</h3>
-      </div>
+      <Home />
     </>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <>
+      <section id="cardSection">
+        <div className="container mx-auto">
+          <div className="box">tes</div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Card;
