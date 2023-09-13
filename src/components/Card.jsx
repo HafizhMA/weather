56464b04f11506ce5c57a7f654053dd37c772950
HafizhMA <muhammadazhr29@gmail.com>
2023-09-13 19:45:02 +0700

    install axios

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 
 const Card = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lakukan sesuatu ketika tombol "Cari" ditekan, misalnya mengirimkan data pencarian ke server.
+  };
   return (
-    <>
-      <section id="cardSection">
-        <div className="container mx-auto">
-          <div className="box">tes</div>
+    <section
+      id="cardSection"
+      className="h-screen flex justify-center items-center"
+    >
+      <div className="boxContainer container mx-auto rounded">
+        <div className="searchBox w-64 mx-auto">
+          <form className="flex" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="w-full px-4 py-2 rounded-l-full focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
+              placeholder="Cari..."
+            />
+            <button
+              type="submit"
+              className="px-4 py-2 bg-blue-500 text-white rounded-r-full hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+            >
+              Cari
+            </button>
+          </form>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
